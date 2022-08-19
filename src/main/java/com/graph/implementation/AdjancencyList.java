4a97826d89931cee11a6755b1209dedc2835f566
adityamulik <aditya.mulik@gmail.com>
2022-08-19 21:35:40 +0530

    added missing file for graph

---
+++ src/main/java/com/graph/implementation/AdjancencyList.java
@@ -0,0 +1,36 @@
+package com.graph.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.linkedlists.implementation.SinglyLinkedList;
+
+public class AdjancencyList {
+    /*
+     * Adjacency List representation is used with an int data and
+     * it's associated edges made up of a SinglyLinkedList Array
+     */
+    private List<Integer> vertices;
+    private SinglyLinkedList[] edges;
+    private int vertexCount;
+
+    public AdjancencyList(int vertexCount) {
+        this.vertexCount = vertexCount;
+        this.vertices = new ArrayList<>();
+        this.edges = new SinglyLinkedList[vertexCount];
+        for (int i=0; i<vertices.size(); i++) {
+            vertices.add(i);
+            edges[i] = new SinglyLinkedList();
+        }
+    }
+
+    public void addEdge(int source, int destination) {
+        int i = vertices.indexOf(source);
+        int j = vertices.indexOf(destination);
+
+        if (i != -1 || j != -1) {
+            edges[i].insertAtBeginning(destination);
+            edges[i].insertAtBeginning(source);
+        }
+    }
+}
