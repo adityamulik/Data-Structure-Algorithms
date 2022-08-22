453e36860567d1e1fc4c47575af2d380de994f1b
adityamulik <aditya.mulik@gmail.com>
2022-08-22 20:17:56 +0530

    test play

--- src/main/java/com/dsa/App.java
+++ src/main/java/com/dsa/App.java
@@ -1,5 +1,8 @@
 package com.dsa;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int[][] matrix = {{2,6},{1,3},{15,18},{8,10}};
+        sort2DMatrixByColZero(matrix);
+        for (int[] row: matrix) {
+            System.out.println(Arrays.toString(row));
+        }        
+    }
+
+    public static void sort2DMatrixByColZero(int[][] matrix) {
+
+        Arrays.sort(matrix, new Comparator<int[]>() {
+            
+            @Override
+            public int compare(final int[] entry1, final int[] entry2) {
+                if (entry1[0] > entry2[0]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
     }
 }
